b2c77b470e030f1e377990d060001252bd3db9e4
Alexey Kovrizhkin <jean@tender.pro>
2015-05-05 20:31:24 +0300

    added dist build mode

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-rm dbcc*
-#gox -osarch="linux/amd64"
-gox -os="linux"
+DIST=$1
+
+for f in dbcc* ; do [ -f $f ] && rm $f ; done
+
+if [[ "$DIST" ]] ; then
+  gox -os="linux"
+else
+  gox -osarch="linux/amd64"
+fi
+
 sha256sum dbcc* > SHA256SUMS
